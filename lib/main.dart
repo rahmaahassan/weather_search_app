5826fb466dff99b6ab6eebf6b318d6ecc06e90c8
rahmahassan <rh51796@gmail.com>
2021-01-12 15:40:40 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'bloc/weather_bloc.dart';
+import 'data/weather_repository.dart';
+import 'pages/weather_search_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      home: BlocProvider(
+        create: (BuildContext context) => WeatherBloc(FakeWeatherRepository()),
+        child: WeatherSearchPage(),
+      ),
+    );
+  }
+}
